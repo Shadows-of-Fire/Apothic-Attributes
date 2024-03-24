afcc875d64cb4aa4f395da99a28ad9459e3ae000
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-03-24 16:10:19 -0700

    Fix tridents not respecting arrow damage
    Closes https://github.com/Shadows-of-Fire/Apotheosis/issues/1203

---
+++ src/main/java/dev/shadowsoffire/attributeslib/mixin/ThrownTridentMixin.java
@@ -0,0 +1,24 @@
+package dev.shadowsoffire.attributeslib.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.ThrownTrident;
+import net.minecraft.world.level.Level;
+
+@Mixin(ThrownTrident.class)
+public abstract class ThrownTridentMixin extends AbstractArrow {
+
+    protected ThrownTridentMixin(EntityType<? extends AbstractArrow> type, Level level) {
+        super(type, level);
+    }
+
+    @ModifyConstant(method = "onHitEntity(Lnet/minecraft/world/phys/EntityHitResult;)V", constant = @Constant(floatValue = 8.0F))
+    public float apoth_getTridentDamage(float defaultDmg) {
+        return (float) (this.getBaseDamage() * 4.0F);
+    }
+
+}
